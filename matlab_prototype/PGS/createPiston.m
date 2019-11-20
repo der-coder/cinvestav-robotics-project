5000abb7c26cab035745b9b3ea87685ef81669fb
Enrique Benavides Tellez <enrique.b.tellez@gmail.com>
2019-11-19 18:47:50 -0600

    PGS simulink
     Conflicts:
    	matlab_prototype/PGS/PlatGoughStewart.slx
    	matlab_prototype/PGS/Q_LIK.m
    	matlab_prototype/PGS/plat_prop.m
    	matlab_prototype/PGS/slprj/_jitprj/jitEngineAccessInfo.mat
    	matlab_prototype/PGS/slprj/_jitprj/sWtPD3ipS4QOHu63SuXUHJB.l
    	matlab_prototype/PGS/slprj/_jitprj/sWtPD3ipS4QOHu63SuXUHJB.mat
    	matlab_prototype/PGS/slprj/_sfprj/EMLReport/emlReportAccessInfo.mat
    	matlab_prototype/PGS/slprj/_sfprj/EMLReport/sWtPD3ipS4QOHu63SuXUHJB.mat
    	matlab_prototype/PGS/slprj/_sfprj/PlatGoughStewart/_self/sfun/info/binfo.mat
    	matlab_prototype/PGS/slprj/_sfprj/precompile/FEchqg6bbVXEa5V5vxHOED.mat
    	matlab_prototype/PGS/slprj/_sfprj/precompile/ULzU4xEL21xZbIvntvoJoH.mat
    	matlab_prototype/PGS/slprj/_sfprj/precompile/autoInferAccessInfo.mat
    
     It looks like you may be committing a cherry-pick.
     If this is not correct, please remove the file
    	.git/CHERRY_PICK_HEAD
     and try again.
    
     Please enter the commit message for your changes. Lines starting
     with '#' will be ignored, and an empty message aborts the commit.
    
     Author:    Enrique Benavides Tellez <enrique.b.tellez@gmail.com>
     Date:      Tue Nov 19 18:47:50 2019 -0600
    
     On branch isaac
     Your branch is up to date with 'origin/isaac'.
    
     You are currently cherry-picking commit e4e63c6.
    
     Changes to be committed:
    	modified:   .gitignore
    	new file:   matlab_prototype/PGS/PlatGoughStewart.slx
    	new file:   matlab_prototype/PGS/Q_LIK.m
    	modified:   matlab_prototype/PGS/createPiston.m
    	new file:   matlab_prototype/PGS/datospos.png
    	new file:   matlab_prototype/PGS/plat_prop.m
    	new file:   matlab_prototype/PGS/pose_M.mat

--- matlab_prototype/PGS/createPiston.m
+++ matlab_prototype/PGS/createPiston.m
@@ -3,56 +3,12 @@ function [inertiaPiston, mass, distanceCenterMass] = createPiston()
 % Medido desde el sistema de coordenadas de salida.
 % Mediciones de Solidworks
 
-	Lxx = 703671034.55;	
-    Lxy = 370.37;
-    Lxz = 0.48;
-	Lyx = 370.37;
-    Lyy = 2079848.98;
-    Lyz = -0.25;
-	Lzx = 0.48;	
-    Lzy = -1.00;
-    Lzz = 703687095.27;
-    
-    
     convertGramMM2toKgM2 = (1/1000)*(1/1000)*(1/1000);
     
-    inertiaPiston = convertGramMM2toKgM2 * [Lxx , -Lxy , -Lxz; -Lyx, Lyy, -Lyz; -Lzx, -Lzy , Lzz ];
     
-    mass = 5.48131;
+    inertiaPiston = ;
     
-    distanceCenterMass = [0; 460.39; 0]*(1/1000);
+    mass = ;
+    
+    distanceCenterMass = ;
 end
-
-
-% Propiedades de masa de actuator
-%      Configuración:  Predeterminado
-%      Sistema de coordenadas:  -- predeterminado --
-% 
-% Masa = 5481.31 gramos
-% 
-% Volumen = 918344.47 milímetros cúbicos
-% 
-% Área de superficie = 362362.67  milímetros cuadrados
-% 
-% Centro de masa: ( milímetros )
-% 	X = 0.00
-% 	Y = 460.39
-% 	Z = 0.00
-% 
-% Ejes principales de inercia y momentos principales de inercia: ( gramos *  milímetros cuadrados )
-% Medido desde el centro de masa.
-% 	 Ix = ( 0.00,  1.00,  0.00)   	Px = 2079848.97
-% 	 Iy = (-1.00,  0.00,  0.00)   	Py = 703671034.54
-% 	 Iz = ( 0.00,  0.00,  1.00)   	Pz = 703687095.27
-% 
-% Momentos de inercia: ( gramos *  milímetros cuadrados )
-% Obtenidos en el centro de masa y alineados con el sistema de coordenadas de resultados.
-% 	Lxx = 703671034.55	Lxy = 370.37	Lxz = 0.48
-% 	Lyx = 370.37	Lyy = 2079848.98	Lyz = -1.00
-% 	Lzx = 0.48	Lzy = -1.00	Lzz = 703687095.27
-% 
-% Momentos de inercia: ( gramos *  milímetros cuadrados)
-% Medido desde el sistema de coordenadas de salida.
-% 	Ixx = 1865493625.74	Ixy = -244.20	Ixz = 0.48
-% 	Iyx = -244.20	Iyy = 2079848.98	Iyz = -0.25
-% 	Izx = 0.48	Izy = -0.25	Izz = 1865509686.46
---
+++ matlab_prototype/PGS/plat_prop.m
@@ -0,0 +1,29 @@
+function [anga,angb,a,b,lmin] = plat_prop()
+rb = .5;                        % Radio de la junta en la base
+ra = .3;                        % Radio de la junta en la plataforma
+lmin = 1;                       % Extensión minima del piston/actuador
+hb = .1;                        % Separación lineal entre juntas en la base
+ha = .1;                        % Separacion lineal entre juntas en la plataforma
+% Iniciar los valores de la plataforma
+
+th_b = asin(hb/(2*rb));
+th_a = asin(ha/(2*ra));
+
+angb = zeros(6,1);
+anga = zeros(6,1);
+for i = 1:6
+  anga(i,1) = ((2*pi)/3)*floor((i+1)/2) - (pi/3) + (-1)^i*th_a;
+  angb(i,1) = ((2*pi)/3)*floor(i/2) - (-1)^i*th_b;
+end
+
+a = zeros(3,6);
+b = zeros(3,6);
+for i = 1:6
+  a(1,i) = ra*cos(anga(i));
+  a(2,i) = ra*sin(anga(i));
+  a(3,i) = 0;
+  b(1,i) = rb*cos(angb(i));
+  b(2,i) = rb*sin(angb(i));
+  b(3,i) = 0;
+end
+end
